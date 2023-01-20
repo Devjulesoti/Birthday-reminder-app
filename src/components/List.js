c41dc3535006da4641b8ac6cced71d9cb8287c29
juliet oti <egwuotijulietchinenye@gmail.com>
2023-01-20 03:57:55 +0100

    First commit

---
+++ src/components/List.js
@@ -0,0 +1,26 @@
+import React from "react";
+import '../styles/list.css'
+
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        const { id, firstName, lastName, age, image } = person;
+        return(
+        <article className="person" key={id}>
+          <img src={image} alt={firstName} />
+          <div>
+            <h5>
+             {firstName}
+            </h5>
+            <h5>{lastName}</h5>
+            <p>{age} years</p>
+          </div>
+        </article>
+        )
+      })}
+    </>
+  );
+};
+
+export default List;
